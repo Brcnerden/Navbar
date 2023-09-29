8af8c9b02322f5ee397e6f596b11eaeaaf60bf75
Brcnerden <brcnerden@gmail.com>
2023-09-29 11:19:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function toogleClick() {
+  let mobileList = document.getElementById("mobileList");
+  let list = document.getElementById("list");
+
+  mobileList.classList.toggle("open");
+  list.classList.toggle("close");
+}
